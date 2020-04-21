2ebda8734bdb5f505ab4631917069db9d24876db
pranaliandre <pranaliandre06@gmail.com>
2020-04-21 00:51:04 -0500

    Added: Welcome to census analyser

---
+++ censusAnalyser/censusAnalyser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace censusAnalyser
+{
+    class censusAnalyser
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!!!Welcome to census analyser!!!");
+        }
+    }
+}
